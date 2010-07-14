635dc0410f3aaa2bcdb78fe4ee453861ace1c090
Stew Benedict <stewb@linuxfoundation.org>
2010-07-14 15:49:18 -0400

    Add path selector (bug 480)

--- search/views.py
+++ search/views.py
@@ -3,7 +3,7 @@ import os, re, urllib
 from django.conf import settings
 from django.shortcuts import render_to_response
 from django import forms
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 from janitor.site_settings import gui_name, gui_version
 from janitor.search.models import *
@@ -117,6 +117,31 @@ def documentation(request):
                               {'name': gui_name, 
                                'version': gui_version, 
                                'gui_docs': docs })
+
+# this does not have a corresponding dirlist.html
+# this is dynamic filetree content fed to jqueryFileTree for the test.html file/dir selection
+# script for jqueryFileTree points to /linkage/dirlist/
+def dirlist(request):
+    # filter out some directories that aren't useful from "/"
+    not_wanted = [ '/proc', '/dev', '/sys', '/initrd' ]
+    r=['<ul class="jqueryFileTree" style="display: none;">']
+    try:
+        d=urllib.unquote(request.POST.get('dir'))
+        content = os.listdir(d)
+        # slows things a little, but looks more like 'ls'
+        for f in sorted(content, key=unicode.lower):
+            ff=os.path.join(d,f)
+            if ff not in not_wanted and f != 'lost+found':
+                if os.path.isdir(ff): 
+                    r.append('<li class="directory collapsed"><a href="#" rel="%s/">%s</a></li>' % (ff,f))
+                else:
+                    e=os.path.splitext(f)[1][1:] # get .ext and remove dot
+                    r.append('<li class="file ext_%s"><a href="#" rel="%s">%s</a></li>' % (e,ff,f))
+        r.append('</ul>')
+    except Exception,e:
+        r.append('Could not load directory: %s' % str(e))
+    r.append('</ul>')
+    return HttpResponse(''.join(r))
 
 ## utility functions
 
