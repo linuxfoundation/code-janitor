6a4d2269dfcae84e5cb13238bcb05e459caea7f7
Jeff Licquia <jeff@licquia.org>
2010-07-23 15:04:03 -0400

    Start of a test framework, with one working test.

--- search/tests.py
+++ search/tests.py
@@ -1,23 +1,13 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from janitor.search import models
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class TestSearch(TestCase):
+    fixtures = ["test_keyword_data.xml"]
 
->>> 1 + 1 == 2
-True
-"""}
+    def testSearch(self):
+        s = models.Search(top_path="./testdata/search_test.txt")
+        s.save()
+        s.do()
 
+        self.assertTrue(s.searchitem_set.count() > 0)
